ecf1de57d1ad7aff7d0a99908e93907f88b6bb50
Linx <olga-reshetova@mail.ru>
2023-11-16 14:17:08 +0300

    верстка модального окна с формой. добавлен js. рефакторинг

--- source/js/app.js
+++ source/js/app.js
@@ -13,3 +13,45 @@ mainNavToggle.addEventListener('click', () =>
     }
   }
 );
+
+const consultation = document.querySelector('.consultation');
+const consultationContent = document.querySelector('.consultation__content');
+const consultationAnswer = document.querySelector('.consultation__answer');
+const consultationButton = document.querySelector('.consultation__button');
+
+consultationButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  if (consultation.classList.contains('consultation--content-opened')) {
+    consultation.classList.remove('consultation--content-opened');
+    consultation.classList.add('consultation--answer-opened');
+  }
+});
+
+
+const modal = document.querySelector('.modal');
+const modalButton = document.querySelector('.modal__button');
+
+modalButton.addEventListener('click', () => {
+  if (modal.classList.contains('modal--closed')) {
+    modal.classList.remove('modal--closed');
+    modal.classList.add('modal--opened');
+  } else {
+    modal.classList.add('modal--closed');
+    modal.classList.remove('modal--opened');
+  }
+
+  if (consultation.classList.contains('consultation--answer-opened')) {
+    consultation.classList.remove('consultation--answer-opened');
+    // consultation.classList.add('consultation--content-opened');
+  }
+});
+
+const callButtons= document.querySelectorAll(['.app-button--call', '.app-button--staff']);
+
+callButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    modal.classList.remove('modal--closed');
+    modal.classList.add('modal--opened');
+  });
+})
